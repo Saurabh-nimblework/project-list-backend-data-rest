3c38a992009c59b661e313c4fde60968d954bedf
Saurabh-nimblework <saurabh.nandwal@nimblework.com>
2023-09-04 00:12:18 +0530

    final-rest-api

--- src/main/java/com/engineering/restapi/projectlistbackenddatarest/ProjectListRepo.java
+++ src/main/java/com/engineering/restapi/projectlistbackenddatarest/ProjectListRepo.java
@@ -1,7 +1,11 @@
 package com.engineering.restapi.projectlistbackenddatarest;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "*")
+@RepositoryRestResource(collectionResourceRel = "list", path = "list")
 public interface ProjectListRepo extends MongoRepository<ProjectList, String> {
     // Define custom query methods if needed
 
